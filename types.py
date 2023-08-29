53a2bcb917ab0c1258e8644c0a1c1206358bf0e8
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2023-08-29 11:55:44 +0100

    updated

--- types.py
+++ types.py
@@ -16,3 +16,11 @@ def string_to_list(s, cast=None):
         l = [ i.strip() for i in s.strip().strip('][').split(',') ]
     return l
 
+
+class PrintableList(list):
+    def __str__(self):
+        out = ""
+        for i in self.__iter__():
+            out += '%i\n' %i
+        out = out[:-1] # to remove the last \n
+        return out
